cbdbcb1ccd62537d92c1c20cd2635208628abf03
Diego Modesto <diegosanches89@gmail.com>
2025-06-21 21:27:52 -0300

    database connection and api testing

--- lib/address_book_web/router.ex
+++ lib/address_book_web/router.ex
@@ -7,15 +7,11 @@ defmodule AddressBookWeb.Router do
 
   scope "/api", AddressBookWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:address_book, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
